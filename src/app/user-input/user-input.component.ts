6d6022d4afca5b775ce94599aec06342a30a77e7
VoinaRares <voinarares14@gmail.com>
2024-10-11 15:13:11 +0300

    first commit

---
+++ src/app/user-input/user-input.component.ts
@@ -0,0 +1,27 @@
+import { Component, EventEmitter, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import type { InvestmentInput } from '../../investment.model';
+
+@Component({
+  selector: 'app-user-input',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './user-input.component.html',
+  styleUrl: './user-input.component.css'
+})
+export class UserInputComponent {
+  @Output() calculate = new EventEmitter<InvestmentInput>();
+  initialInvestment = '0';
+  annualInvestment = '0';
+  expectedReturn ='5';
+  duration = '10';
+
+  onSubmit(){
+    this.calculate.emit({initialInvestment: +this.initialInvestment,
+      duration: +this.duration,
+      expectedReturn: +this.expectedReturn,
+      annualInvestment: +this.annualInvestment,
+    });
+  }
+
+}
